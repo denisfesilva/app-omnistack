153efeaf508173504e1ab7aee603e143d0b24f15
Denis Feliciano <denisf.salles@gmail.com>
2019-10-02 20:10:32 -0300

    criado o frontend, e algumas paginas, sendo elas, dashboard, new e a pag princioal

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+
 const routes = require('./routes');
+
 const app = express();
 
 
@@ -16,7 +21,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-otri6.mongodb.net/
 // req.params = acessar route params (para edição e delete)
 // req.body = acessar corpo da requisição (para criação, edição de registros)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
